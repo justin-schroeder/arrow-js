cee98ddb63388c6b580dd86c68583b5ea3599a99
RafalKornel <rafal.kornel123@gmail.com>
2022-11-19 23:49:14 +0100

    test: add test case for bug

---
+++ test/w.test.ts
@@ -0,0 +1,55 @@
+import { nextTick, r, w } from '../src'
+
+type Data = {
+  value: number | null
+}
+
+describe('w', () => {
+  it('should call when depencency changes', async () => {
+    const d = r({ value: 0 })
+
+    const callback = jest.fn(() => {
+      d.value
+    })
+
+    w(callback)
+
+    await nextTick()
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    d.value = 10
+    await nextTick()
+
+    expect(callback).toHaveBeenCalledTimes(2)
+
+    d.value = 20
+    await nextTick()
+
+    expect(callback).toHaveBeenCalledTimes(3)
+  })
+
+  it('should handle null values', async () => {
+    const d = r<Data>({ value: null })
+
+    const inner = jest.fn()
+
+    function callback() {
+      if (d.value !== null) {
+        inner()
+      }
+    }
+
+    w(callback)
+
+    await nextTick()
+    expect(inner).toHaveBeenCalledTimes(0) // while value is null, inner should not be called
+
+    d.value = 10
+    await nextTick()
+    expect(inner).toHaveBeenCalledTimes(1)
+
+    d.value = null
+    await nextTick()
+    expect(inner).toHaveBeenCalledTimes(1)
+  })
+})
