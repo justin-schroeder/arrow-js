798b29b9f11fc3a70d9ecedfa32e014f2b00ae42
Giga <gigabyte5671@gmail.com>
2022-11-24 15:01:23 +1300

    Add Carousel to the demos navigation

--- docs/demos/components/demos.js
+++ docs/demos/components/demos.js
@@ -3,6 +3,7 @@ import { t } from '/dev/index.js'
 const demos = [
   { href: '/demos/fast-text.html', label: 'DOM Patching' },
   { href: '/demos/calculator.html', label: 'Calculator' },
+  { href: '/demos/carousel.html', label: 'Carousel' },
   { href: '/demos/dropdowns.html', label: 'Dropdowns' }
 ]
 
